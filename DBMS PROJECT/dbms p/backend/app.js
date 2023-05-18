c905c2f97b4faec2bcad065de93df906934d6217
Guru1826 <imguru1826@gmail.com>
2023-05-18 19:20:44 +0530

    trail change

--- DBMS PROJECT/dbms p/backend/app.js
+++ DBMS PROJECT/dbms p/backend/app.js
@@ -35,7 +35,7 @@ app.use("/api/v1/poc",pocRouter);
 
 
 app.get("/", (req, res) => {
-  res.send("Nice working");
+  res.send("Nice working"); jefknq;4ajejfjaeij ioejrofioi
 });
 
 // Using Error Middleware
